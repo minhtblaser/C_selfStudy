af9616f01ff7a02489a495140872828dda0701ff
minhtblaser <trinhngocminh.9a1@gmail.com>
2021-01-20 15:51:01 +0700

    thêm code v1: - nhập 2 số - in 2 số - hoán vị 2 số - tìm và in các phép tính - tìm và in max

--- src/Number_v0.cpp
+++ src/Number_v0.cpp
@@ -1,17 +1,81 @@
-#include<stdio.h>
-void enter(int &first, int &second);
+#include <stdio.h>
+void enterByReference(int &first, int &second);
+void enterByValue(int *first, int *second);
+void print(const int first, const int second);
+void swap (int &first, int &second);
+void swap (int *first, int *second);
+void printCaculate(const int first, const int second);
+void printMax(const int first, const int second);
+int plus(const int first, const int second);
+int minus(const int first, const int second);
+int multiple(const int first, const int second);
+float divide(const int first, const int second);
+int findMax(const int first, const int second);
 int main(){
-    int first,second;
-    enter(first,second);
+    int first, second;
+    enterByReference(first, second);
+    // enterByValue(&first, &second);
+    print(first, second);
+    // swap(first, second);
+    // printf("\nAfter swap by Reference {first number:%d, second number: %d}", first, second);
+    swap(&first, &second);
+    printf("\nAfter swap by Value {first number:%d, second number: %d}", first, second);
+    printf("\n-----------------");
+    printCaculate(first, second);
+    printMax(first, second);
     return 0;
 }
-void enter(int &first, int &second){
-    printf("Enter first number: ");
-    scanf("%d",&first);
-    printf("Enter second number: ");
-    scanf("%d",&second);
-<<<<<<< HEAD
-} // :P :P :P :P
-=======
+void enterByReference(int &first, int &second){
+    printf("\nenter the first number: ");
+    scanf("%d", &first);
+    printf("\nenter the second number: ");
+    scanf("%d", &second);
 }
->>>>>>> a5019906b55435811687331406975cdab94b76c9
+void enterByValue(int *first, int *second){
+    printf("\nenter the first number (value): ");
+    scanf("%d", first);
+    printf("\nenter the second number(value): ");
+    scanf("%d", second);
+}
+void print(const int first, const int second){
+    printf("\n{first number:%d, second number: %d}", first, second);
+}
+void swap (int &first, int &second){
+    int temp;
+    temp = first;
+    first = second;
+    second = temp;
+}
+void swap(int *first, int *second){
+    int temp;
+    temp = *first;
+    *first = *second;
+    *second = temp;
+}
+int plus(const int first, const int second){
+    return first + second;
+}
+int minus(const int first, const int second){
+    return first - second;
+}
+int multiple(const int first, const int second){
+    return first * second;
+}
+float divide(const int first, const int second){
+    return (float)first / second;
+}
+void printCaculate(const int first, const int second){
+    int sum = plus(first, second);
+    int subtraction = minus(first, second);
+    int product = multiple(first, second);
+    float quotient = divide(first, second);
+    printf("\n{summary: %d, substraction:%d, product:%d, quotient: %.2f}\n", sum, subtraction, product, quotient);
+    printf("-----------------------------------------\n");
+}
+int findMax(const int first, const int second){
+    return (first > second) ? first : second;
+}
+void printMax(const int first, const int second){
+    int result_max = findMax(first, second);
+    printf("\nmax is %d ", result_max);
+}
