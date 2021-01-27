ef6861c7e58847a7af4365f08c9a904a593fd43f
thaitam <phithaitam@gmail.com>
2021-01-28 02:18:00 +0700

    thêm hàm tính toán

--- answer/number/number_1.cpp
+++ answer/number/number_1.cpp
@@ -3,7 +3,12 @@
 void enter(int &first, int &second);// NOTE: nhập vào 2 số sử dụng tham chiếu
 void enter(int *first, int *second);// NOTE: nhập vào 2 số sử dụng con trỏ
 void swap(int *first, int *second);// NOTE: hoán vị 2 số
+int add(const int first, const int second);// NOTE: tính tổng 2 số
+int sub(const int first, const int second);// NOTE: tính hiệu 2 số
+int mul(const int first, const int second);// NOTE: tính tích 2 số
+float divide(const int first, const int second);// NOTE: tính thương 2 số
 void print(const int first,const int second);// NOTE: in ra 2 số
+void printCaculator(const int first,const int second);// NOTE: in biểu thức
 int main(){
     /* nhập vào 2 số sử dụng biến chứa giá trị
      int first, second;// chua gia tri
@@ -18,6 +23,7 @@ int main(){
     print(*first,*second);
     swap(first, second);
     print(*first,*second);
+    printCaculator(*first,*second);
     return 0;
 }
 void enter(int &first, int &second){
@@ -44,4 +50,25 @@ void swap(int *first, int *second){
 }
 void print(const int first,const int second){
     printf("{first: %d,second: %d}\n",first,second);
+}
+int add(const int first, const int second){
+    return first + second;
+}
+int sub(const int first, const int second){
+    return first - second;
+}
+int mul(const int first, const int second){
+    return first * second;
+}
+float divide(const int first, const int second){
+    return (float)first/second;
+}
+void printCaculator(const int first,const int second){
+    int sum = add(first,second);
+    int diff = sub(first,second);
+    int multiply = mul(first,second);
+    float division = divide(first,second);
+    printf("--------------------\n");
+    print(first,second);
+    printf("caculator:{sum: %d, different:%d, multiply:%d, division:%.2f}",sum,diff,multiply,division);
 }
